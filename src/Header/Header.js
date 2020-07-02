8fcf0e7a784b14c6e28bd20f5998937c7999b88c
Marli Renee <marlirenee@gmail.com>
2020-07-02 15:21:07 -0700

    initial commit. still recieving some errors.

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render () {
+        return (
+            <div className="App-header">
+                <header>
+                    <h1>
+                        Noteful
+                    </h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ src/IndividualNote/IndividualNote.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './IndividualNote.css'
+
+class IndividualNote extends Component {
+    render () {
+        return (
+            
+        )
+    }
+}
+
+export default IndividualNote
